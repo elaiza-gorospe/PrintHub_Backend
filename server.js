ad2436dc8e052f7b1af32b1dac8e1f52d52c878f
patrizia-ducut <patriziaducut615@gmail.com>
2026-05-16 08:09:35 +0800

    new updates

--- server.js
+++ server.js
@@ -959,10 +959,6 @@ app.post("/api/reset-password", async (req, res) => {
   }
 });
 
-// START SERVER
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
 
 // -------------------------
 // Orders API
